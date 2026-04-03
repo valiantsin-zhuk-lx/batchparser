e6b9f0bcf2a07ce226d72299f2082eaf2f13678c
Valiantsin.Zhuk <Valiantsin.Zhuk@leverx.com>
2026-04-03 11:24:24 +0300

    Build app for GitHub Pages deployment

---
+++ parser/docs/test/unit/controller/Main.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["batch/parser/controller/Main.controller"],function(n){"use strict";QUnit.module("Main Controller");QUnit.test("I should test the Main controller",function(t){var o=new n;o.onInit();t.ok(o)})});
+//# sourceMappingURL=Main.controller.js.map
